b1cf2bc339cbb471a96d57d906ca5f354cb0982f
Volkovsky Dima <dimkk@outlook.com>
2017-03-26 17:28:49 +0300

    mongodump fix

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ AZ_STORAGE_CS=${AZ_STORAGE_CS}
 [[ ( -n "${MONGODB_PASS}" ) ]] && PASS_STR=" --password ${MONGODB_PASS}"
 [[ ( -n "${MONGODB_DB}" ) ]] && DB_STR=" --db ${MONGODB_DB}"
 
-BACKUP_CMD="mongodump --archive /backup/${BACKUP_NAME} --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
+BACKUP_CMD="mongodump --archive=/backup/${BACKUP_NAME} --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
 
 echo "=> Creating backup script"
 rm -f /backup.sh
